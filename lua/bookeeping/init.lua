1fcb67b701219f62a5b0d831e4372af444faa2fc
Thalles Oliveira <thalles.gbpereira@gmail.com>
2025-05-28 22:48:27 -0300

    Fixed incorrect keyboard shortcut for opening plugin

--- lua/bookeeping/init.lua
+++ lua/bookeeping/init.lua
@@ -1,7 +1,7 @@
 local Bookeeping = {}
 
 function Bookeeping.setup()
-    vim.api.nvim_set_keymap("n", "<leader>n", "<Cmd>lua require('bookeeping.ui').create_window()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>b", "<Cmd>lua require('bookeeping.ui').create_window()<CR>", { noremap = true, silent = true })
 
     vim.api.nvim_create_user_command("Bookeeping", function()
         require("bookeeping.ui").create_window()
